71b1d7099b6240b77fbf75003b935e37aeac5fbe
monsharen <github@opt.nu>
2023-02-03 10:43:21 +0100

    proj

---
+++ EndlessRunnerServer/EndlessRunnerServer.cs
@@ -0,0 +1,12 @@
+﻿using CloudCode.Authoring.Modules;
+
+namespace EndlessRunnerServer;
+
+public class EndlessRunnerServer
+{
+    [CloudCodeFunction("HelloWorld")]
+    public string HelloWorld()
+    {
+        return "Hello world!";
+    }
+}
